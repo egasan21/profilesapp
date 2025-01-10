5219986ee649d67a95da8149af8a04530d1f6f9e
egasan21 <140286610+egasan21@users.noreply.github.com>
2025-01-10 02:29:20 -0500

    displaying user profile

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>
+);
